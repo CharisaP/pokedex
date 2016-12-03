44b5e3043e91db78b69a286d7afc213a3d2cd4d4
pnr14b <pnr14b@my.fsu.edu>
2016-12-02 20:18:54 -0500

    Add files via upload

--- schema.sql
+++ schema.sql
@@ -3,17 +3,24 @@
 drop table if exists users;
     create table users (
     username text primary key not null,
-    password text  null
+    password text null,
+    FOREIGN KEY(username) REFERENCES OwnedPokemon(owner)
 );
 
-drop table if exists pokemon;
-    create table pokemon (
-
+drop table if exists OwnedPokemon;
+    create table OwnedPokemon (
+    type text,	
     pname text not null,
-    pid integer primary key not null,
-    ptype text not null,
-    owner text not null
-
+    level int,
+    owner text
+    PRIMARY KEY(owner,pname)
     );
 
-    
+drop table if exists PokemonDB;
+    create table PokemonDB (
+	type text,
+	name text,
+	PRIMARY KEY(name,type),
+	FOREIGN KEY(name)
+	REFERENCES OwnedPokemon(pname)
+	);
