097771ff6eab0b687148a3b1e16605b596ac0cad
Victor C <fvlse@users.noreply.github.com>
2016-12-04 21:14:16 -0500

    Add files via upload

--- models.py
+++ models.py
@@ -1,69 +1,102 @@
 import sqlite3 as sql
+from sqlite3 import OperationalError
 
 def addUser(username,password):
     con = sql.connect("database.db")
     cur = con.cursor()
-    cur.execute("INSERT INTO users (username,password) VALUES (?,?)", (username,password))
-    con.commit()
+    try:
+        cur.execute("INSERT INTO users (username,password) VALUES (?,?)", (username,password))
+        con.commit()
+        
+    except OperationalError:
+        pass
     con.close()
 
 def retrieveUsers():
     con = sql.connect("database.db")
     cur = con.cursor()
-    cur.execute("SELECT username, password FROM users")
-    users = cur.fetchall()
-    con.close()
-    return users
-
-def retrieveEligibleUsers():
-    con = sql.connect("database.db")
-    cur = con.cursor()
-    cur.execute("SELECT username FROM users")
-    users = cur.fetchall()
+    users = []
+    try:
+        cur.execute("SELECT username, password FROM users")
+        users = cur.fetchall()
+        
+    except OperationalError:
+        pass
     con.close()
     return users
 
 def addPokemon(pname,ptype,owner,plevel):
     con = sql.connect("database.db")
     cur = con.cursor()
-    cur.execute("INSERT INTO OwnedPokemon (name,type,owner,level) VALUES (?,?,?,?)", (pname,ptype,owner,plevel))
-    con.commit()
+    try:
+        cur.execute("INSERT INTO OwnedPokemon (name,type,owner,level) VALUES (?,?,?,?)", (pname,ptype,owner,plevel))
+        con.commit()
+    except OperationalError:
+        pass
     con.close()
 
 def getPokeList():
     con = sql.connect("database.db")
     cur = con.cursor()
-    cur.execute("SELECT * FROM Pokemon")
-    pokemon = cur.fetchall()
+    pokemon = []
+    try:
+        cur.execute('SELECT * FROM Pokemon')
+        pokemon = cur.fetchall()
+    except OperationalError:
+        pass
     con.close()
     return pokemon
-
+    
 def eraseUserPokemon(user,mon):
     con = sql.connect("database.db")
     cur = con.cursor()
-    cur.execute("DELETE FROM OwnedPokemon WHERE owner = ? AND name = ?",(user,mon,))
-    con.commit()
+    try:
+        cur.execute('DELETE FROM OwnedPokemon WHERE owner = ? and name = ?',(user,mon))
+        con.commit()
+    except OperationalError:
+        pass
     con.close()
 
 def searchByType(typename):
     con = sql.connect("database.db")
     cur = con.cursor()
-    cur.execute("SELECT name FROM Pokemon WHERE  type = ?",(typename,))
-    pokemon = cur.fetchall()
+    pokemon = []
+    try:
+        cur.execute("SELECT name FROM Pokemon WHERE  type = ?",(typename,))
+        pokemon = cur.fetchall()
+    except OperationalError:
+        pass
     con.close()
     return pokemon
 
 def eraseUsers():
     con = sql.connect("database.db")
     cur = con.cursor()
-    cur.execute("DELETE * FROM users")
-    con.commit()
+    try:
+        cur.execute("DELETE * FROM users")
+        con.commit()
+    except OperationalError:
+        pass
     con.close()
 
 def userPokemon(user):
     con = sql.connect("database.db")
     cur = con.cursor()
-    cur.execute('SELECT name,type,level FROM OwnedPokemon Where owner=?',(user,))
-    mons = cur.fetchall()
+    mons =  []
+    try:
+        cur.execute('SELECT name,type,level FROM OwnedPokemon Where owner=?',(user,))
+        mons = cur.fetchall()
+    except OperationalError:
+        pass
     con.close()
     return mons
+
+def updateLevel(newLevel,name,owner):
+    con = sql.connect("database.db")
+    cur = con.cursor()
+    try:
+        cur.execute('UPDATE OwnedPokemon SET level = ? WHERE name = ? and owner = ?',(newLevel,name,owner))
+        con.commit()
+    except OperationalError:
+        pass
+    con.close()
